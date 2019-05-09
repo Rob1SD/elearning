5294f9866d651e5e315bdba3176742d8f54793e1
Robin Solde <robin.solde@gfi.fr>
2019-05-09 14:59:22 +0200

    on degage le timer en dur, et on check quand la video est fini

--- vodeclic.js
+++ vodeclic.js
@@ -1,43 +1,74 @@
-
-var divVideo=$("#global > article > div > div.formation > div.h-slider > div > div:nth-child(2) > div.list-videos.skill-list > div.list-videos-container");
+var selector="#global > article > div > div.formation > div.h-slider > div > div:nth-child(2) > div.list-videos.skill-list > div.list-videos-container"
+var divVideo=$(selector);
 var divVideoChilds = divVideo[0].childNodes;
 var divList=[];
 var allVideos=divVideo.find("ul");
 var VideoList=[]
-var pos=0; // la video a jouer en premier
-
-// temps en minute
-var tempsEntreDeuxVideo = 10;
-var timer = tempsEntreDeuxVideo * 60;
+var pos=0;
 
 
-for (var i = 0; i < divVideoChilds.length; ++i){
-	if (divVideoChilds[i].classList && divVideoChilds[i].classList.value == "skill-list-item closed"){
-		divVideoChilds[i].click();
-		divList.push(divVideoChilds[i]);
+var fillVideos = function(){
+	divVideo=$(selector);
+	divVideoChilds = divVideo[0].childNodes;
+	allVideos=divVideo.find("ul");
+	divList=[];
+	VideoList=[]
+	for (var i = 0; i < divVideoChilds.length; ++i){
+		if (divVideoChilds[i].classList && divVideoChilds[i].classList.value.indexOf("skill-list-item") >=0 ){
+			divList.push(divVideoChilds[i]);
+		}
 	}
-}
 
-for (var i = 0; i < allVideos.length; ++i){
-	var allLi=allVideos[i].children;
-	for (var j = 0; j < allLi.length; ++j){
-		VideoList.push(allLi[j]);
+	for (var i = 0; i < allVideos.length; ++i){
+		var allLi=allVideos[i].children;
+		for (var j = 0; j < allLi.length; ++j){
+			VideoList.push(allLi[j]);
+		}
+	}
+
+
+}
+var isSeen = function(video){
+	for (var i = 0; i < video.children.length; ++i){
+		if (video.children[i].dataset && video.children[i].dataset.msvSeen){
+			
+			for (var j = 0; j < video.children[i].classList.length; ++j){
+				if (video.children[i].classList[j] == "v-active")
+					return true;
+				
+				
+			}
+			//return false;
+		}
+	}
+	return false;
+}
+fillVideos();
+for (var i = 0; i < VideoList.length; ++i){
+	if (isSeen(VideoList[i])){
+		pos++;
 	}
 }
 setTimeout(function() {
+	console.log("on demarre video : " + pos);
 	VideoList[pos].click();
-	pos++;
+	
 },2000);
 setInterval(function() {
-	VideoList[pos].click();
-	pos++;
-	timer=tempsEntreDeuxVideo*60;
-},tempsEntreDeuxVideo * 60 * 1000);
-
-setInterval(function() {
-	console.log("changement de video dans : " + timer);
-	timer--;
+	fillVideos();
 	
-},1000);
+	if (isSeen(VideoList[pos]))
+	{
+		pos++;
+		console.log("next video");
+		VideoList[pos].click();
+	}
+	else {
+		
+		console.log("video pas terminée");
+		
+	}
+},10000);
+
 
 
